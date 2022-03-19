54e13be000e1691a425f8310ff95d39d22f2f43f
Balajti <83246230+Balajti@users.noreply.github.com>
2022-03-19 23:52:30 +0100

    web

---
+++ app.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function ($) {
+    $(".open-contact-popup").click(function () {
+      $(".contact-popup").addClass("open");
+    });
+  
+    $(".close-contact-popup").click(function () {
+      $(".contact-popup").removeClass("open");
+    });
+  });
+  
+  gsap.registerPlugin(ScrollTrigger);
+  
+  function initScrollAnimations() {
+    gsap.to(".contact-popup", {
+      scrollTrigger: {
+        trigger: ".four",
+        start: () => `top+=${innerWidth * 3} center`,
+        end: () => `top+=${innerWidth * 4} center`,
+        toggleClass: { targets: ".contact-popup", className: "open" },
+        scrub: true,
+        markers: true,
+        scroller: ".page-content",
+        invalidateOnRefresh: true
+      }
+    });
+  }
+  
+  function init() {
+    initScrollAnimations();
+  }
+  
+  window.addEventListener("load", function () {
+    init();
+  });
